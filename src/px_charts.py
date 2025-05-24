d371343ef9c2e49daf3135000271ae648c5d841e
lynn-twinkl <lynnmatildap@gmail.com>
2025-05-24 18:32:27 -0500

    added: topic count plot

--- src/px_charts.py
+++ src/px_charts.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import plotly.express as px
 
-def plot_hist(df: pd.DataFrame, col_to_plot: str, bins: int, height: int = 500):
+def plot_histogram(df: pd.DataFrame, col_to_plot: str, bins: int, height: int = 500):
 
     plt = px.histogram(
             df,
             x=col_to_plot,
             nbins=bins,
+            title=None,
             color_discrete_sequence=['#646DEF']
             )
 
@@ -14,5 +15,49 @@ def plot_hist(df: pd.DataFrame, col_to_plot: str, bins: int, height: int = 500):
             bargap=0.1,
             height=height
             )
+
+    return plt
+
+
+# =========== TOPIC DISTRIBUTION CHART  ===========
+
+
+def plot_topic_countplot(topics_df: pd.DataFrame, topic_id_col: str, topic_name_col: str, representation_col: str, height: int = 500):
+    """
+    This functions plots a count chart for Bertopic topics,
+    extracting the 5 words of each topic's representation
+    in order to provide more context
+    """
+
+    ## ----- Extract top 5 words ----
+    topics_df['top_5_words'] = topics_df[representation_col].apply(lambda x: ", ".join(x[:5]) if isinstance(x, list) else x)
+
+    plt = px.bar(
+            topics_df,
+            x=topic_id_col,
+            y='Count',
+            custom_data=["top_5_words", topic_name_col],
+            title=None,
+            color_discrete_sequence=['#FF5733']
+            )
+
+    plt.update_traces(
+        marker_color='#646DEF',
+        textposition='outside',
+        hovertemplate=(
+            'Topic Name: %{customdata[1]}<br>'
+            'Frequency: %{y}<br>'
+            'Top 5 words: %{customdata[0]}<extra></extra>'
+            )
+        )
+
+    plt.update_layout(
+        uniformtext_minsize=10,
+        height=height,
+        hoverlabel=dict(
+            font_size=14
+        )
+    )
+
 
     return plt
